d2536e06ada8d6a858b0edde2d7c59ece56d3b30
ThijnBr <thijnbroekhuizen@hotmail.com>
2023-11-23 15:24:36 -0500

    final

--- GPIO/tatata/tatata.py
+++ GPIO/tatata/tatata.py
@@ -2,23 +2,14 @@ from machine import Pin
 import time
 
 pin = Pin(20,Pin.OUT)
-kort = 0.3
-lang = 0.75
 
-while True:
-    pin.value(0)
-    time.sleep(kort)
-    pin.value(1)
-    time.sleep(kort)
-    pin.value(0)
-    time.sleep(kort)
-    pin.value(1)
-    time.sleep(kort)
-    pin.value(0)
-    time.sleep(kort)
-    pin.value(1)
-    time.sleep(kort)
+def pulse(kort,lang):
     pin.value(0)
     time.sleep(kort)
     pin.value(1)
     time.sleep(lang)
+
+while True:
+    for x in range(3):
+        pulse(0.3, 0.75)
+    pulse(1, 2)
